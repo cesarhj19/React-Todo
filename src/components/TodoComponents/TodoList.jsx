70fbf0ac1e28f940a8fdf71260e32394e94da36b
cesarhj19 <cesarhj19@gmail.com>
2020-01-15 20:39:03 -0800

    MVP Completed

---
+++ src/components/TodoComponents/TodoList.jsx
@@ -0,0 +1,22 @@
+// your components will all go in this `component` directory.
+// feel free to change this component.js into TodoList.js
+import React, { Component } from 'react';
+import Todo from './Todo';
+
+class TodoList extends Component {
+	render() {
+		return (
+			<div>
+				{this.props.todos.map(todo => (
+					<Todo
+						key={todo.id}
+						todo={todo}
+						handleTodoCompleted={this.props.handleTodoCompleted}
+					/>
+				))}
+			</div>
+		);
+	}
+}
+
+export default TodoList;
